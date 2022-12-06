d39edd86daa6ae22bbe8d2d22979e8fdf08bf27e
Jusley Souza <jusleysouza31@gmail.com>
2022-12-06 10:15:25 -0300

    Refactoring code

--- src/test/java/com/ju/springboot/reactive/ReactivedemoApplicationTests.java
+++ src/test/java/com/ju/springboot/reactive/ReactivedemoApplicationTests.java
@@ -12,12 +12,13 @@ class ReactivedemoApplicationTests {
 	@Test
 	void testMono() {
 		Mono<String> mono = Mono.just("MacBook Pro");
-		mono.log().map(data -> data.toUpperCase()).subscribe(data -> System.out.println(data));
+		mono.log().map(data -> data.toUpperCase()).subscribe(System.out::println);
 	}
 
 	@Test
 	void testFlux() {
-		Flux.just("MacBook Pro", "Iphone", "Dell").log().map(data -> data.toUpperCase()).subscribe(data -> System.out.println(data));
+		Flux.just("MacBook Pro", "Iphone", "Dell").log().map(data -> data.toUpperCase())
+		.subscribe(new OrderConsumer());
 	}
 	
 }
