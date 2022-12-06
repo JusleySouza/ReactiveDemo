308e7ce829514b84a76d8b8d9b2a4e32494dea66
Jusley Souza <jusleysouza31@gmail.com>
2022-12-06 11:12:35 -0300

    Using subscrible methods.

--- src/test/java/com/ju/springboot/reactive/ReactivedemoApplicationTests.java
+++ src/test/java/com/ju/springboot/reactive/ReactivedemoApplicationTests.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import reactor.core.publisher.Flux;
@@ -24,7 +26,28 @@ class ReactivedemoApplicationTests {
 		Flux.fromIterable(Arrays.asList("MacBook Pro", "Iphone", "Dell"))
 		.delayElements(Duration.ofSeconds(2))
 		.log().map(data -> data.toUpperCase())
-		.subscribe(new OrderConsumer());
+		.subscribe(new Subscriber<String>() {
+
+			@Override
+			public void onSubscribe(Subscription subscription) {
+				subscription.request(Long.MAX_VALUE);
+			}
+
+			@Override
+			public void onNext(String order) {
+				System.out.println(order);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+			}
+
+			@Override
+			public void onComplete() {
+				System.out.println("Completely Done!!");
+			}
+		});
 		
 		Thread.sleep(6000);
 	}
