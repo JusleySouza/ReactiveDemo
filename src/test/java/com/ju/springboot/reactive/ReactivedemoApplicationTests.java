fb1193789d6fd5d4d2ce67c79d3a41b9320126a7
Jusley Souza <jusleysouza31@gmail.com>
2022-12-06 10:07:21 -0300

    Implementing the Flux test

--- src/test/java/com/ju/springboot/reactive/ReactivedemoApplicationTests.java
+++ src/test/java/com/ju/springboot/reactive/ReactivedemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.ju.springboot.reactive;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootTest
@@ -11,7 +12,12 @@ class ReactivedemoApplicationTests {
 	@Test
 	void testMono() {
 		Mono<String> mono = Mono.just("MacBook Pro");
-		mono.log().map(data -> data.toUpperCase());
+		mono.log().map(data -> data.toUpperCase()).subscribe(data -> System.out.println(data));
 	}
 
+	@Test
+	void testFlux() {
+		Flux.just("MacBook Pro", "Iphone", "Dell").log().map(data -> data.toUpperCase()).subscribe(data -> System.out.println(data));
+	}
+	
 }
